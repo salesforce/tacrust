b3c0ffdfd4d805cfca0f013921bebd170952a812
Rahul Yadav <rahulyadav@rahulya-ltmwpjj.internal.salesforce.com>
2021-12-01 22:38:43 +0000

    Authentication Continue packet type, serialization and de-ser

--- tests/parser_tests.rs
+++ tests/parser_tests.rs
@@ -54,3 +54,19 @@ pub fn test_bad_key() {
 
     assert_eq!(result.is_err(), true);
 }
+
+#[test]
+pub fn test_packet_authen_continue() {
+    let key = "tackey".as_bytes();
+    let reference_packet = include_bytes!("../packets/sample_packet_authen_reply_2");
+    println!("\n >> Reference Packet\n");
+    println!("{}", pretty_hex(reference_packet));
+
+    let (_, parsed_packet) = parser::parse_packet(reference_packet, key).unwrap();
+    println!("\n >> Parsed\n");
+    println!("{:#?}", parsed_packet);
+
+    let serialized = serializer::serialize_packet(&parsed_packet, key).unwrap();
+    println!("\n >> Serialized\n");
+    println!("{}", pretty_hex(&serialized));
+}
