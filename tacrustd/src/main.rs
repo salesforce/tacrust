5a333d705d3862afbc88580a068d09325fc11ef2
Kamran Khan <k.khan@salesforce.com>
2022-01-06 12:15:12 -0800

    Move config to twelf

--- tacrustd/src/main.rs
+++ tacrustd/src/main.rs
@@ -1,60 +1,36 @@
-use clap::Parser;
+use clap::App;
 use color_eyre::Report;
-use figment::{
-    providers::{Env, Format, Serialized, Toml},
-    Figment,
-};
-use serde::{Deserialize, Serialize};
 use tracing::info;
 use tracing_subscriber::EnvFilter;
-
-#[derive(Deserialize, Serialize)]
-struct Config {
-    key: String,
-    another: u32,
-}
-
-impl Default for Config {
-    fn default() -> Config {
-        Config {
-            key: "default".into(),
-            another: 100,
-        }
-    }
-}
+use twelf::{config, Layer};
 
 /// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[clap(about, version, author)]
-struct Args {
+#[config]
+struct Conf {
     /// Name of the person to greet
-    #[clap(short, long)]
     name: String,
 
     /// Number of times to greet
-    #[clap(short, long, default_value_t = 1)]
     count: u8,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
-    let config = setup()?;
-    let args = Args::parse();
+    let app = clap::App::new("tacrust").args(&Conf::clap_args());
+    let config = setup(&app)?;
 
-    info!("[Config] key={}, another={}", config.key, config.another);
-
-    if args.count > 100 {
+    if config.count > 100 {
         panic!("Are you crazy?");
     }
 
-    for _ in 0..args.count {
-        info!("Hello {}!", args.name)
+    for _ in 0..config.count {
+        info!("Hello {}!", config.name)
     }
 
     Ok(())
 }
 
-fn setup() -> Result<Config, Report> {
+fn setup(app: &App) -> Result<Conf, Report> {
     if std::env::var("RUST_LIB_BACKTRACE").is_err() {
         std::env::set_var("RUST_LIB_BACKTRACE", "1")
     }
@@ -67,10 +43,12 @@ fn setup() -> Result<Config, Report> {
         .with_env_filter(EnvFilter::from_default_env())
         .init();
 
-    let figment = Figment::from(Serialized::defaults(Config::default()))
-        .merge(Toml::file("tacrust.toml"))
-        .merge(Env::prefixed("TACRUST_"));
-    let config = figment.extract()?;
+    let config = Conf::with_layers(&[
+        Layer::Toml("tacrust.toml".into()),
+        Layer::Env(Some("TACRUST_".to_string())),
+        Layer::Clap(app.get_matches().clone()),
+    ])
+    .unwrap();
 
     Ok(config)
 }
