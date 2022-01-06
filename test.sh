897b101eeb5cde082aa7840eed5129f6b862f300
Kamran Khan <k.khan@salesforce.com>
2022-01-06 13:42:10 -0800

    Do not use grcov for now

--- test.sh
+++ test.sh
@@ -6,9 +6,14 @@ export TOKEN="66632e33-d018-4a35-afce-3cafcac256cc"
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 make
-grcov ./target/debug -t coveralls -s . --token $TOKEN --ignore 'vendor/*' --ignore '/rustc/*' > coveralls.json
+
+coverage () {
+	grcov ./target/debug -t coveralls -s . --token $TOKEN --ignore 'vendor/*' --ignore '/rustc/*' > coveralls.json
+	bash <(curl -s https://codecov.moe.prd-sam.prd.slb.sfdc.net/bash) -t $TOKEN -f coveralls.json -Z
+}
 
 if [ "$STRATA" == "yes" ]; then
-	bash <(curl -s https://codecov.moe.prd-sam.prd.slb.sfdc.net/bash) -t $TOKEN -f coveralls.json -Z
+	echo "Testing code coverage"
+	# coverage
 fi
 
