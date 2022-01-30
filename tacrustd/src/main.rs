b6b772daaa212661fa5613ab287a485ff5851fac
Kamran Khan <k.khan@salesforce.com>
2022-01-29 22:43:57 -0800

    Create a channel for graceful shutdown of server

--- tacrustd/src/main.rs
+++ tacrustd/src/main.rs
@@ -6,6 +6,8 @@ use futures::{SinkExt, StreamExt};
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use std::{path::Path, sync::Arc};
+use tokio::sync::mpsc::UnboundedSender;
+use tokio::task::JoinHandle;
 use tokio::{
     net::{TcpListener, TcpStream},
     sync::RwLock,
@@ -83,9 +85,15 @@ pub struct Config {
 
 #[tokio::main]
 async fn main() -> Result<(), Report> {
+    let (join_handle, _cancel_tx) = start_server().await?;
+    join_handle.await?;
+
+    Ok(())
+}
+
+async fn start_server() -> Result<(JoinHandle<()>, UnboundedSender<()>), Report> {
     let config = Arc::new(setup()?);
     let state = Arc::new(RwLock::new(State::new(config.key.as_bytes().to_vec())));
-    let listener = TcpListener::bind(&config.listen_address).await?;
 
     if config.acls.is_some() {
         let mut state = state.write().await;
@@ -112,27 +120,40 @@ async fn main() -> Result<(), Report> {
     tracing::debug!("state: {:?}", state.read().await);
     tracing::info!("listening on {}", &config.listen_address);
 
-    loop {
-        tokio::select! {
-            result = listener.accept() => {
-                let (stream, addr) = result?;
-                let state = Arc::clone(&state);
+    let listener = TcpListener::bind(&config.listen_address).await?;
+    let (tx, mut rx) = tokio::sync::mpsc::unbounded_channel::<()>();
+    let join_handle = tokio::spawn(async move {
+        loop {
+            tokio::select! {
+                result = listener.accept() => {
+                    let (stream, addr) = match result {
+                        Ok(v) => v,
+                        Err(e) => {
+                            tracing::info!("error accepting connection; error = {}", e);
+                            continue
+                        },
+                    };
+                    let state = Arc::clone(&state);
 
-                tokio::spawn(async move {
-                    tracing::debug!("accepted connection");
-                    if let Err(e) = process(state, stream, addr).await {
-                        tracing::info!("an error occurred; error = {}", e);
-                    }
-                });
-            }
-            _ = tokio::signal::ctrl_c() => {
-                tracing::info!("received ctrl-c, exiting");
-                break;
+                    tokio::spawn(async move {
+                        tracing::debug!("accepted connection");
+                        if let Err(e) = process(state, stream, addr).await {
+                            tracing::info!("an error occurred; error = {}", e);
+                        }
+                    });
+                }
+                _ = rx.recv() => {
+                    tracing::info!("received channel req to shutdown, exiting");
+                    break;
+                }
+                _ = tokio::signal::ctrl_c() => {
+                    tracing::info!("received ctrl-c, exiting");
+                    break;
+                }
             }
         }
-    }
-
-    Ok(())
+    });
+    Ok((join_handle, tx))
 }
 
 fn setup() -> Result<Config, Report> {
