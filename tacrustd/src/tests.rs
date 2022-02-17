4ea95eb05c33012ed5ece24b0c55e32d80fa74db
Kamran Khan <k.khan@salesforce.com>
2022-02-16 19:46:15 -0800

    Refactor tests to eliminate duplicate code

--- tacrustd/src/tests.rs
+++ tacrustd/src/tests.rs
@@ -69,6 +69,68 @@ fn get_tcp_client_for_tacrust() -> TcpStream {
     stream
 }
 
+fn test_authen_packet(packet: &[u8], key: &[u8], expected_status: AuthenticationStatus) {
+    let mut client = get_tcp_client_for_tacrust();
+    tracing::info!(
+        "sending packet: {}",
+        Base64Display::with_config(packet, base64::STANDARD)
+    );
+    client.write(packet).unwrap();
+    let mut response = [0; 4096];
+    tracing::info!("receiving response");
+    let len = client.read(&mut response).unwrap();
+    tracing::info!(
+        "received response: {}",
+        Base64Display::with_config(&response[0..len], base64::STANDARD)
+    );
+    let (_, parsed_response) = tacrust::parser::parse_packet(&response[0..len], key).unwrap();
+    tracing::info!("parsed: {:?}", parsed_response);
+    assert!(matches!(
+        parsed_response.body,
+        Body::AuthenticationReply { .. }
+    ));
+    if let Body::AuthenticationReply {
+        status,
+        flags: _,
+        server_msg: _,
+        data: _,
+    } = parsed_response.body
+    {
+        assert_eq!(status, expected_status);
+    }
+}
+
+fn test_author_packet(packet: &[u8], key: &[u8], expected_status: AuthorizationStatus) {
+    let mut client = get_tcp_client_for_tacrust();
+    tracing::info!(
+        "sending packet: {}",
+        Base64Display::with_config(packet, base64::STANDARD)
+    );
+    client.write(packet).unwrap();
+    let mut response = [0; 4096];
+    tracing::info!("receiving response");
+    let len = client.read(&mut response).unwrap();
+    tracing::info!(
+        "received response: {}",
+        Base64Display::with_config(&response[0..len], base64::STANDARD)
+    );
+    let (_, parsed_response) = tacrust::parser::parse_packet(&response[0..len], key).unwrap();
+    tracing::info!("parsed: {:?}", parsed_response);
+    assert!(matches!(
+        parsed_response.body,
+        Body::AuthorizationReply { .. }
+    ));
+    if let Body::AuthorizationReply {
+        status,
+        data: _,
+        server_msg: _,
+        args: _,
+    } = parsed_response.body
+    {
+        assert_eq!(status, expected_status);
+    }
+}
+
 #[test]
 #[serial]
 fn server_startup_and_shutdown() {
@@ -86,32 +148,7 @@ fn test_java_author() {
     let port: u16 = rand::thread_rng().gen_range(10000..30000);
     test_server(port, Duration::from_secs(1), || {
         let packet = include_bytes!("../packets/java-author-1.tacacs");
-        let mut client = get_tcp_client_for_tacrust();
-        tracing::info!(
-            "sending packet: {}",
-            Base64Display::with_config(packet, base64::STANDARD)
-        );
-        client.write(packet).unwrap();
-        let mut response = [0; 4096];
-        tracing::info!("receiving response");
-        let len = client.read(&mut response).unwrap();
-        tracing::info!(
-            "received response: {}",
-            Base64Display::with_config(&response[0..len], base64::STANDARD)
-        );
-        let (_, parsed_response) = tacrust::parser::parse_packet(&response[0..len], key).unwrap();
-        tracing::info!("parsed: {:?}", parsed_response);
-        match parsed_response.body {
-            Body::AuthorizationReply {
-                status,
-                data: _,
-                server_msg: _,
-                args: _,
-            } => {
-                assert_eq!(status, AuthorizationStatus::AuthStatusFail);
-            }
-            _ => tracing::info!("invalid response"),
-        }
+        test_author_packet(packet, key, AuthorizationStatus::AuthStatusFail);
     });
 }
 
@@ -122,65 +159,13 @@ fn test_golang_authen() {
     let port: u16 = rand::thread_rng().gen_range(10000..30000);
     test_server(port, Duration::from_secs(1), || {
         {
-            let mut client = get_tcp_client_for_tacrust();
             let packet = include_bytes!("../packets/golang-authen-1.tacacs");
-            tracing::info!(
-                "sending packet: {}",
-                Base64Display::with_config(packet, base64::STANDARD)
-            );
-            client.write(packet).unwrap();
-            let mut response = [0; 4096];
-            tracing::info!("receiving response");
-            let len = client.read(&mut response).unwrap();
-            tracing::info!(
-                "received response: {}",
-                Base64Display::with_config(&response[0..len], base64::STANDARD)
-            );
-            let (_, parsed_response) =
-                tacrust::parser::parse_packet(&response[0..len], key).unwrap();
-            tracing::info!("parsed: {:?}", parsed_response);
-            match parsed_response.body {
-                Body::AuthenticationReply {
-                    status,
-                    flags: _,
-                    server_msg: _,
-                    data: _,
-                } => {
-                    assert_eq!(status, AuthenticationStatus::GetPass);
-                }
-                _ => tracing::info!("invalid response"),
-            }
+            test_authen_packet(packet, key, AuthenticationStatus::GetPass);
         }
 
         {
-            let mut client = get_tcp_client_for_tacrust();
             let packet = include_bytes!("../packets/golang-authen-2.tacacs");
-            tracing::info!(
-                "sending packet: {}",
-                Base64Display::with_config(packet, base64::STANDARD)
-            );
-            client.write(packet).unwrap();
-            let mut response = [0; 4096];
-            tracing::info!("receiving response");
-            let len = client.read(&mut response).unwrap();
-            tracing::info!(
-                "received response: {}",
-                Base64Display::with_config(&response[0..len], base64::STANDARD)
-            );
-            let (_, parsed_response) =
-                tacrust::parser::parse_packet(&response[0..len], key).unwrap();
-            tracing::info!("parsed: {:?}", parsed_response);
-            match parsed_response.body {
-                Body::AuthenticationReply {
-                    status,
-                    flags: _,
-                    server_msg: _,
-                    data: _,
-                } => {
-                    assert_eq!(status, AuthenticationStatus::Pass);
-                }
-                _ => tracing::info!("invalid response"),
-            }
+            test_authen_packet(packet, key, AuthenticationStatus::Pass);
         }
     });
 }
